f854801666520cd30b3ab3ed7f4040297420251b
ManasC478 <manas.chougule@gmail.com>
2025-04-08 21:12:34 -0700

    feat: connect db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,34 @@
 import express from "express";
+import cors from "cors";
+import db from "./lib/db";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello, Bun with TypeScript & Express!");
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const users = await db("users").select("*");
+  res.status(200).json({ users });
+});
+
+app.post("/event", async (req, res) => {
+  try {
+    const [event] = await db("events").insert({
+      ...req.body,
+    }).returning("*");
+    res.status(201).json({ event });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
-
